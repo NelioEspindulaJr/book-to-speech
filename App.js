a27579ecdf2417e71fa3c3ff3d858df4f01abbfc
Nélio Espíndula Junior <nelio.espindula@zig.tickets>
2024-03-16 20:02:19 -0300

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Dimensions, Text, View, TouchableOpacity } from "react-native";
+import {
+  useCameraDevice,
+  useFrameProcessor,
+  Camera,
+} from "react-native-vision-camera";
+
+const { width } = Dimensions.get("window");
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [words, setWords] = useState([]);
+  const device = useCameraDevice("back");
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermission();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  useEffect(() => {
+    console.log(words);
+  }, [words]);
+
+  const handleBarCodeScanned = ({ data }) => {
+    setWords((prevWords) => [...prevWords, data]);
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <Camera
+        style={{ width: width, height: width }}
+        device={device}
+        isActive={true}
+      >
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "transparent",
+            flexDirection: "row",
+          }}
+        ></View>
+      </Camera>
+      <View>
+        <Text>{words}</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
